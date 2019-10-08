bdfd5d16c73faff4369808720b96735ccd4865be
miia <miia@com.tw>
2019-10-08 16:38:52 +0800

    bmi

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edweight;
+    private EditText edheight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edweight = findViewById(R.id.ed_weight);
+        edheight = findViewById(R.id.ed_height);
+
+    }
+
+    public void bmi (View view){
+        float weight = Float.parseFloat(edweight.getText().toString());
+        float height = Float.parseFloat(edheight.getText().toString());
+        float bmi = weight/(height*height);
+        Log.d("Fuck", "bmi: "+bmi);
+
+    }
+}
